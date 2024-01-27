fdfd7541b8a472c67df7d4f41855b7d5e3593a7f
paoloricciuti <ricciutipaolo@gmail.com>
2024-01-27 22:32:57 +0100

    feat: skew selection towards upvotes

--- src/lib/db/queries/index.ts
+++ src/lib/db/queries/index.ts
@@ -1,16 +1,30 @@
-import { eq } from 'drizzle-orm';
+import { eq, sql, desc } from 'drizzle-orm';
 import { db } from '..';
 import { movies } from '../schemas/movies';
 import { get_skewed_random } from '$lib/utils';
 import type { Film } from '$lib/validations';
+import { upvotes } from '../schemas/upvotes';
 
 export async function get_emojis_from_title_from_db(title: string) {
-	// TODO: sort by upvotes
-	const movie_list = await db.select().from(movies).where(eq(movies.title, title));
+	const movie_list = await db
+		.select({
+			movies: {
+				id: movies.id,
+				emojis: movies.emojis,
+				title: movies.title,
+			},
+			upvotes: sql`cast(count(${upvotes.id}) as int)`,
+		})
+		.from(movies)
+		.leftJoin(upvotes, eq(movies.id, upvotes.for_movie))
+		.where(eq(movies.title, title))
+		.groupBy(movies.id, movies.emojis, movies.title)
+		.orderBy((table) => desc(table.upvotes));
+
 	if (movie_list.length === 0) return;
 
 	const random_entry = get_skewed_random(movie_list.length, Math.random);
-	return movie_list[random_entry].emojis;
+	return movie_list[random_entry].movies.emojis;
 }
 
 export function add_new_movie(movie: Film, emojis: string) {
