1261bb24837044ff4c191a5dffd99fdebf7991ad
paoloricciuti <ricciutipaolo@gmail.com>
2025-12-28 01:36:12 +0100

    fix: duh

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,7 +4,8 @@ import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.DATABASE_TOKEN) throw new Error('DATABASE_TOKEN is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ url: env.DATABASE_URL, authToken: env.DATABASE_TOKEN });
 
 export const db = drizzle(client, { schema });
