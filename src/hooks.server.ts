416394c9be293207b2d548bc42215652c4fe8b84
paoloricciuti <ricciutipaolo@gmail.com>
2025-12-28 02:21:45 +0100

    fix: remove logs

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,5 @@ import { isAuthPath } from 'better-auth/svelte-kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 export async function handle({ event, resolve }) {
-	console.log(event.url.toString(), auth.options, isAuthPath(event.url.toString(), auth.options));
 	return svelteKitHandler({ event, resolve, auth, building });
 }
